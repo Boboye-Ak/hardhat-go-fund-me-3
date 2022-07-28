895fe2ef12f7dc130b1f648a227ca51f33c01f3c
Boboye-Ak <boboye771@gmail.com>
2022-07-28 23:44:09 +0100

    My very first commit

---
+++ contracts/Cause.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+contract Cause {
+    //State Variables
+    uint256 public s_causeBalance;
+    uint256 public immutable i_goal;
+    bool public s_isGoalReached;
+    bool public s_isOpenToDonations;
+    address public s_causeOwner;
+    string public s_causeName;
+    mapping(address => uint256) public donorToAmountDonated;
+    address[] public donorList;
+
+    //Custom Errors
+    error Cause__IsNotOpenToDonations();
+    error Cause__GoalAlreadyReached();
+
+    //Events
+    event DonationMade(address indexed donor, uint256 amount);
+
+    //Constructor
+    constructor(
+        string memory causeName,
+        uint256 goal,
+        address payable causeOwner
+    ) {
+        s_causeName = causeName;
+        s_causeOwner = causeOwner;
+        i_goal = goal;
+        s_isOpenToDonations = true;
+    }
+
+    //Receive and Fallback Functions
+
+    //Pure functions
+    function donate() public payable {
+        if (s_isGoalReached) {
+            revert Cause__GoalAlreadyReached();
+        }
+        if (!s_isOpenToDonations) {
+            revert Cause__IsNotOpenToDonations();
+        }
+
+        s_causeBalance += msg.value;
+        donorList.push(msg.sender);
+        donorToAmountDonated[msg.sender] += msg.value;
+        if (s_causeBalance >= i_goal) {
+            s_isGoalReached = true;
+            s_isOpenToDonations = false;
+        }
+        emit DonationMade(msg.sender, msg.value);
+    }
+}
