45abb2e7e31157229e8c31c27c94a6ce26403f78
Boboye-Ak <boboye771@gmail.com>
2022-08-04 15:27:42 +0100

    arranged staging test

--- test/staging tests/crowdfunder.staging.test.js
+++ test/staging tests/crowdfunder.staging.test.js
@@ -7,12 +7,28 @@ const { deploy, log } = deployments
 developmentChains.includes(network.name)
     ? describe.skip
     : describe("CrowdFunder", () => {
-          let crowdFunder, deployer
+          //account 0 deploys CrowdFunder,
+          //account 1 creates cause,
+          //accounts 2, 3 and 4 donate to the cause
+          //and account 1 withdraws
+          let crowdFunder, deployer, accounts
           const chainId = network.config.chainId
           const causeName = "My Cause Name"
           const goal = networkConfig[chainId]["goal"]
           beforeEach(async () => {
               deployer = (await getNamedAccounts()).deployer
               crowdFunder = await ethers.getContract("CrowdFunder", deployer)
+              accounts = await ethers.getSigners()
+          })
+          it("Creates a cause, takes donations and allows the owner to withdraw from the cause", async () => {
+            //create cause
+            const causeCrowdFunder=await crowdFunder.connect(accounts[1])
+            await causeCrowdFunder.createCause(causeName, goal)
+            const latestCauseAddress=await crowdFunder.getLatestCauseAddress()
+            const causeABI=(await hre.artifacts.readArtifact("Cause")).abi
+            const latestCause = new ethers.Contract(latestCauseAddress, causeABI, accounts[1])
+            const donor1LatestCause=await latestCause.connect(accounts[2])
+            const donor2LatestCause=await latestCause.connect(accounts[3])
+            const donor3LatestCause=await latestCause.connect(accounts[4])
           })
       })
