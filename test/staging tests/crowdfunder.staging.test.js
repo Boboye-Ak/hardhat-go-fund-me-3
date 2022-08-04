b36804414818a42467b9e11cb66bf4fc91d1fc40
Boboye-Ak <boboye771@gmail.com>
2022-08-04 15:42:49 +0100

    acted in staging test

--- test/staging tests/crowdfunder.staging.test.js
+++ test/staging tests/crowdfunder.staging.test.js
@@ -11,24 +11,34 @@ developmentChains.includes(network.name)
           //account 1 creates cause,
           //accounts 2, 3 and 4 donate to the cause
           //and account 1 withdraws
-          let crowdFunder, deployer, accounts
+          let crowdFunder, deployer, accounts, donationValue
           const chainId = network.config.chainId
           const causeName = "My Cause Name"
           const goal = networkConfig[chainId]["goal"]
+          donationValue = ethers.utils.parseEther("1.0")
           beforeEach(async () => {
               deployer = (await getNamedAccounts()).deployer
               crowdFunder = await ethers.getContract("CrowdFunder", deployer)
               accounts = await ethers.getSigners()
           })
           it("Creates a cause, takes donations and allows the owner to withdraw from the cause", async () => {
-            //create cause
-            const causeCrowdFunder=await crowdFunder.connect(accounts[1])
-            await causeCrowdFunder.createCause(causeName, goal)
-            const latestCauseAddress=await crowdFunder.getLatestCauseAddress()
-            const causeABI=(await hre.artifacts.readArtifact("Cause")).abi
-            const latestCause = new ethers.Contract(latestCauseAddress, causeABI, accounts[1])
-            const donor1LatestCause=await latestCause.connect(accounts[2])
-            const donor2LatestCause=await latestCause.connect(accounts[3])
-            const donor3LatestCause=await latestCause.connect(accounts[4])
+              //create cause
+              const causeCrowdFunder = await crowdFunder.connect(accounts[1])
+              await causeCrowdFunder.createCause(causeName, goal)
+              const latestCauseAddress = await crowdFunder.getLatestCauseAddress()
+              const causeABI = (await hre.artifacts.readArtifact("Cause")).abi
+              const latestCause = new ethers.Contract(latestCauseAddress, causeABI, accounts[1])
+              const donor1LatestCause = await latestCause.connect(accounts[2])
+              const donor2LatestCause = await latestCause.connect(accounts[3])
+              const donor3LatestCause = await latestCause.connect(accounts[4])
+              //donors donate to cause
+              await donor1LatestCause.donate({ value: donationValue })
+              await donor2LatestCause.donate({ value: donationValue })
+              await donor3LatestCause.donate({ value: donationValue })
+              const causeInitialBalance = await latestCause.provider.getBalance(latestCause.address)
+              const causeOwnerInitialBalance = await accounts[1].getBalance()
+              //owner withdraws from cause
+              await latestCause.withdraw()
+              const causeOwnerFinalBalance = await accounts[1].getBalance()
           })
       })
